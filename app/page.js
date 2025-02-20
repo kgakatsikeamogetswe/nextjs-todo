b60c8fad0f8a061915fe0767ca7fb78ad2ef6439
Keamogetswe Kgakatsi <stimulatekay11ew@gmail.com>
2025-02-20 07:38:39 -0800

    setting up project & tailwind

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hello world
+      </h1>
+      <h1 className="font-phuduFont">
+        Tailwind has been applied
+      </h1>
+    </div>
+  );
+}
