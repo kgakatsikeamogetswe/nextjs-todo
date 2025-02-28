7d0dbc06092763a363e17234d919113f2ee65881
Keamogetswe Kgakatsi <stimulatekay11ew@gmail.com>
2025-02-28 03:46:37 -0800

    Todo Table

--- app/todos/[id]/page.jsx
+++ app/todos/[id]/page.jsx
@@ -1,10 +1,37 @@
 async function TodosPage(){
-  const response = await fetch('http://localhost:3000/api/todos')
-  const data = await response.json()
-  console.log(data)
+  const response = await fetch('http://localhost:3000/api/todos', {
+    cache: 'no-store',
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+
   return (
-    <div>todo Page</div>
+    <section className="min-h-screen flex items-center justify-center">
+      <table className="w-auto bg-gray-500 border border-white rounded-lg overflow-hidden"> 
+        <thead className="bg-gray-800 text-white uppercase text-sm leading-normal">
+          <tr className="bg-gray-800 text-white uppercase text-sm leading-normal">
+            <th className="py-3 px-6 text-left">ID</th>
+            <th className="py-3 px-6 text-left">Name</th>
+            <th className="py-3 px-6 text-left">Action</th>
+          </tr>
+        </thead>
+        <tbody className="text-white text-sm font-light">
+          {data.todos.map(todo => (
+            <tr key={todo.id} className="border-b border-white hover:bg-gray-600">
+                <td className="py-3 px-6">{todo.id}</td>
+                <td className="py-3 px-6">{todo.name}</td>
+                <td className="py-3 px-6">
+                  <button>Delete</button>
+                </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+    </section>
   )
 }
 
-export default TodosPage
+export default TodosPage;
